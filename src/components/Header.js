1c10520071d036a2fbfa9d92f04c8f79751569a8
hardik06 <hardik06.burlycare@gmail.com>
2023-07-27 00:44:46 +0530

    header Name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import "./Portfolio.css";
 
 const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div">
-          My Portfolio
+          <div className="waviy">
+            <span style={{ "--i": 1 }}>M</span>
+            <span style={{ "--i": 2 }}>Y</span>
+            <br />
+
+            <span style={{ "--i": 3 }}>P</span>
+            <span style={{ "--i": 4 }}>O</span>
+            <span style={{ "--i": 5 }}>R</span>
+            <span style={{ "--i": 6 }}>T</span>
+            <span style={{ "--i": 7 }}>F</span>
+            <span style={{ "--i": 8 }}>O</span>
+            <span style={{ "--i": 9 }}>L</span>
+            <span style={{ "--i": 10 }}>I</span>
+            <span style={{ "--i": 10 }}>O</span>
+          </div>
+
+          {/* My Portfolio */}
         </Typography>
       </Toolbar>
     </AppBar>
