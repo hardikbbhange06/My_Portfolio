8df6e5119fac396839557a0194210b4d8a32c476
hardik06 <hardik06.burlycare@gmail.com>
2023-09-23 19:11:06 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
-import React from 'react';
-import Education from './components/Education';
-import Resume from './components/Resume';
-import Testimonials from './components/Testimonials';
-import './App.css';
+import React from "react";
+import Education from "./components/Education";
+import Resume from "./components/Resume";
+import Testimonials from "./components/Testimonials";
+import "./App.css";
 
-import Header from './components/Header/Header';
-import Introduction from './components/Introduction/Introduction';
-import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
-import AboutMe from './components/AboutMe/AboutMe';
-import Skills from './components/Skills/Skills';
-import Projects from './components/Projects/Projects';
-import Projects1 from './components/Projects/Projects1';
-import WorkExperience from './components/WorkExperience/WorkExperience';
-import Contact from './components/Contact/Contact';
-import Rikam from './components/Rikam';
+import Introduction from "./components/Introduction/Introduction";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Skills from "./components/Skills/Skills";
+import Projects from "./components/Projects/Projects";
+import Projects1 from "./components/Projects/Projects1";
+import WorkExperience from "./components/WorkExperience/WorkExperience";
+import Contact from "./components/Contact/Contact";
+import Rikam from "./components/Rikam";
+import Header from "./components/Header/Header";
 // import GitHubContributions from './components/GithubContributions';
 
 const App = () => {
   return (
     <>
-    {/* <Router>
+      {/* <Router>
     <Routes>
       <Route path="/" element={<Header />} />
       
     </Routes>
 
     </Router> */}
-    <Header />
+      <Header />
       <main>
         <Introduction />
-        <AboutMe   />
+        <AboutMe />
         <Skills />
-        <Projects1/>
+        <Projects1 />
         <WorkExperience />
         {/* <Education /> */}
         <Contact />
         {/* <Resume /> */}
         {/* <Testimonials /> */}
-       {/* <GitHubContributions/> */}
-       <Rikam/>
+        {/* <GitHubContributions/> */}
+        <Rikam />
       </main>
-      <footer>
-        {/* Add footer content here */}
-      </footer>
+      <footer>{/* Add footer content here */}</footer>
     </>
   );
 };
